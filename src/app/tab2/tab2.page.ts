b5667de107ccf67203b4b9ec3575c00f1167632e
Leonardo Nagano <odranoelaero@gmail.com>
2023-09-30 16:24:47 -0300

    primeiro commit

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss', 'tab2.page.css']
+})
+export class Tab2Page {
+
+  constructor() {}
+
+}
